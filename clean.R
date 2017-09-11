3bdf80cb4b5bd8541919af6cf69fe158d502f830
Lynn Bradshaw <readyready15728@gmail.com>
2017-09-11 17:51:52 -0400

    Initial commit on unnormalized

--- clean.R
+++ clean.R
@@ -3,10 +3,29 @@
 raw <- read.csv('raw.csv', na.strings='?')
 
 # Get rid of non-predictive columns
-raw$state <- NULL
-raw$county <- NULL
-raw$community <- NULL
 raw$communityname <- NULL
+raw$state <- NULL
+raw$countyCode <- NULL
+raw$communityCode <- NULL
 raw$fold <- NULL
+
+# Get rid of all predictive columns except ViolentCrimesPerPop
+raw$murders <- NULL
+raw$murdPerPop <- NULL
+raw$rapes <- NULL
+raw$rapesPerPop <- NULL
+raw$robberies <- NULL
+raw$robbbPerPop <- NULL
+raw$assaults <- NULL
+raw$assaultPerPop <- NULL
+raw$burglaries <- NULL
+raw$burglPerPop <- NULL
+raw$larcenies <- NULL
+raw$larcPerPop <- NULL
+raw$autoTheft <- NULL
+raw$autoTheftPerPop <- NULL
+raw$arsons <- NULL
+raw$arsonsPerPop <- NULL
+raw$nonViolPerPop <- NULL
 
 write.csv(raw, 'clean.csv', row.names=F)
